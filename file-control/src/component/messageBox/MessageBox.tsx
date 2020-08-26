891fa2eef0229ab32d84a6460cb626eea8360752
tommy <709177815@qq.com>
2020-08-26 14:56:13 +0800

    add messagebox

---
+++ file-control/src/component/messageBox/MessageBox.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import avatar from "../../asset/akari.jpg";
+import "./messageBox.scss";
+
+export type MessageBoxProps = {
+  firstPart: boolean;
+  message: string;
+};
+
+const MessageBox = (props: MessageBoxProps) => {
+  const messageType = props.firstPart ? "first" : "second";
+
+  return (
+    <div className="message-container">
+      <img src={avatar} width="35px" height="35px" />
+      <div className={`left-arrow ${messageType}`}></div>
+      <div className={`message-content ${messageType}`}>
+        <span className="message">{props.message}</span>
+      </div>
+    </div>
+  );
+};
+
+export default MessageBox;
--- file-control/src/container/chatRoom/chatRoom.tsx
+++ file-control/src/container/chatRoom/chatRoom.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Loading, { LoadingThreeDot } from "../../component/loading/loading";
+import MessageBox,{MessageBoxProps} from '../../component/messageBox/MessageBox';
 
 
 
 const chatRoom = () => {
+  const messageBoxProps: MessageBoxProps = {
+    firstPart:true,
+    message:'adfadf'
+  }
   return (
-    <div style={{ backgroundColor: "snow",padding:'24px' }}>
+    <div style={{ backgroundColor: "snow",padding:'24px 12px',minHeight:'calc(100vh - 112px)' }}>
       asdf
       <Loading />
       <LoadingThreeDot />
+      <MessageBox  {...messageBoxProps}/>
     </div>
   );
 };
