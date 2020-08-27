6a1ef4231856bb6aa962e5f97c679b2c96f9d71b
tommy <709177815@qq.com>
2020-08-27 13:22:08 +0800

    add socket

--- file-control/src/container/chatRoom/chatRoom.tsx
+++ file-control/src/container/chatRoom/chatRoom.tsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Loading, { LoadingThreeDot } from "../../component/loading/loading";
-import MessageBox,{MessageBoxProps} from '../../component/messageBox/MessageBox';
+import MessageBox, {
+  MessageBoxProps,
+} from "../../component/messageBox/MessageBox";
+import { socket } from "../../constanst";
 
-
-
-const chatRoom = () => {
+const ChatRoom = () => {
+  const [connection, setConnection] = useState<SocketIOClient.Socket>();
   let messageBoxProps: MessageBoxProps = {
-    firstPart:true,
-    message:'adfadf'
-  }
+    firstPart: true,
+    message: "adfadf",
+  };
   const result = {
     ...messageBoxProps,
-    firstPart:false
-  }
+    firstPart: false,
+  };
+
+  useEffect(() => {
+    if (!socket.connected) {
+      socket.open();
+    }
+    setConnection(socket);
+    return () => {
+      connection?.disconnect();
+    };
+  }, [connection]);
+
   return (
-    <div style={{ backgroundColor: "snow",padding:'24px 12px',minHeight:'calc(100vh - 112px)' }}>
+    <div
+      style={{
+        backgroundColor: "snow",
+        padding: "24px 12px",
+        minHeight: "calc(100vh - 112px)",
+      }}
+    >
       asdf
       <Loading />
       <LoadingThreeDot />
-      <MessageBox  {...messageBoxProps}/>
-      <MessageBox  {...result}/>
+      <MessageBox {...messageBoxProps} />
+      <MessageBox {...result} />
     </div>
   );
 };
 
-export default chatRoom;
+export default ChatRoom;
